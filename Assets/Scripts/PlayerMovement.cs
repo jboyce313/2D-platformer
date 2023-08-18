08e88a78340ec7078cbb883d7feb65d0b2deca31
Jacob Boyce <jboyce313@gmail.com>
2023-08-18 09:14:37 -0500

    added jumping and falling animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
 
+    // enum is a value type defined by a set of named constants
+    // each option has a corresponding int value, starting at 0
+    private enum MovementState { idle, running, jumping, falling };
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,19 +39,34 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateAnimationState(float dirX)
     {
+        MovementState state;
+
+        // handles running
         if (dirX > 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;
             sprite.flipX = false;
         }
         else if (dirX < 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;
             sprite.flipX = true;
         }
         else
         {
-            anim.SetBool("running", false);
+            state = MovementState.idle;
         }
+
+        // handles jumping and falling
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        anim.SetInteger("state", (int)state);
     }
 }
