db65ab9ca48e6bed3f702a2d97cec09096899651
Jacob Boyce <jboyce313@gmail.com>
2023-08-31 13:13:37 -0500

    added level finish logic and flag animation

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     // don't need serialized field because there is only one audio source component on finish object
     private AudioSource finishSound;
+
+    private bool levelCompleted = false;
 
     private void Start()
     {
@@ -14,15 +17,17 @@ public class Finish : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player" && !levelCompleted)
         {
             finishSound.Play();
-            CompleteLevel();
+            levelCompleted = true;
+            // call CompleteLevel() after 2 seconds
+            Invoke("CompleteLevel", 2f);
         }
     }
 
     private void CompleteLevel()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
