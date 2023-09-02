5b648bf14e327a2a3217db023f43a12d341b27aa
Jacob Boyce <jboyce313@gmail.com>
2023-09-02 08:47:13 -0500

    updated level 3

--- Assets/Scripts/Fan.cs
+++ Assets/Scripts/Fan.cs
@@ -6,15 +6,6 @@ public class Fan : MonoBehaviour
 {
     private Rigidbody2D rb;
     [SerializeField] private float fanForce = 10f;
-    [SerializeField] private AudioSource fanSoundEffect;
-
-    [SerializeField] private float waitTime = .2f;
-    private float timer = 0f;
-
-    private void Start()
-    {
-            fanSoundEffect.Play();
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -31,21 +22,9 @@ public class Fan : MonoBehaviour
 
     private void Update()
     {
-        //PlayFanSoundEffect();
         if (rb != null)
         {
             rb.velocity = new Vector2(rb.velocity.x, fanForce);
-        }
-    }
-
-    private void PlayFanSoundEffect()
-    {
-        timer += Time.deltaTime;
-        if (timer > waitTime)
-        {
-            fanSoundEffect.Play();
-            timer = 0f;
-
         }
     }
     
